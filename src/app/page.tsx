abb954b50238b6babbe51c39b72af32bce5e9036
ggomez0 <gaspargomez2000@outlook.com>
2024-01-29 21:16:18 -0300

    backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import Image from "next/image";
+'use client'
+
 import GitHubCorner from "./githubcorner";
+import ShortUrlModel, {ShortUrl} from "./backend/shorturl";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [shortUrls, setShortUrls] = useState<ShortUrl[]>([]);
+
+  useEffect(() => {
+    // Fetch short URLs when the component mounts
+    const fetchShortUrls = async () => {
+      try {
+        const response = await ShortUrlModel.find();
+        setShortUrls(response);
+      } catch (error) {
+        console.error('Error fetching short URLs:', error);
+      }
+    };
+
+    fetchShortUrls();
+  }, []); // Run the effect only once when the component mounts
+
   return (
     <>
     <GitHubCorner/>
@@ -15,34 +34,42 @@ export default function Home() {
     <span
       className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform"
       >
-      sh.url
+      az.url
     </span>
     <span
       className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"
       ></span>
-</h1>
+    </h1>
           <p className="">a new shortener url</p>
+            <form action="/shortUrls" method="POST">
           <div className="flex justify-around mt-8">
-            <input className="text-2xl text-black" type="URL" />
-            <button className="bg-blue-500 hover:bg-blue-700  font-bold py-2 px-4 rounded">
-              shorten
-            </button>
+              <input className="text-2xl text-black" type="URL" />
+              <button className="bg-blue-700 hover:bg-blue-500  font-bold py-2 px-4 rounded">
+                shorten
+              </button>
           </div>
+            </form>
         </div>
 
         <table className="table-auto m-auto">
           <thead>
             <tr className="bg-slate-800">
               <th className="border px-4 py-2">Original</th>
-              <th className="border px-4 py-2">Shortened</th>
+              <th className="border px-4 py-2">Shortened</th>              
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="border px-4 py-2">https://nextjs.org</td>
-              <td className="border px-4 py-2">sh.url/1</td>
-              </tr>
-          </tbody>
+          {shortUrls.map((shortUrl) => (
+                <tr key={shortUrl._id}>
+                  <td>
+                    <a href={shortUrl.full}>{shortUrl.full}</a>
+                  </td>
+                  <td>
+                    <a href={shortUrl.short}>{shortUrl.short}</a>
+                  </td>
+                </tr>
+              ))}
+        </tbody>
         </table>
         
       </div>
